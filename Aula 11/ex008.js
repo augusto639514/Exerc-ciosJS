1e906a2d2930866a89eb2d4a28bf3dad5837adf8
Augusto Baroni Simionato <72254418+augusto639514@users.noreply.github.com>
2021-02-13 18:51:23 -0300

    Add files via upload

---
+++ Aula 11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log(`sua velocidade é ${vel}Km/h`)
+if (vel > 60) {
+    console.log(`Você passou do limite de velocidade, MULTADO!`)
+}
+console.log(`Dirija sempre usando o cinto de segurança`)
